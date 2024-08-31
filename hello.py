8c40639861b35ede037a3c4ed7487f9a000280f6
Serapio <swerapiodela@gmail.com>
2024-08-31 13:30:31 -0700

    add two lines

--- hello.py
+++ hello.py
@@ -2,6 +2,8 @@ def main():
     print('Hello World')
     print('Hello World')
     print('Hello World')
+    print('Hello World')
+    print('Hello World')
 
 if __name__ == '__main__':
     main()
