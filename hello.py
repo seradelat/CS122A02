9b80623f625e056636d48bd76f656137b5839e13
serap <swerapiodela@gmail.com>
2024-08-31 13:37:16 -0700

    error on line 7

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ def main():
     print('Hello World')
     print('Hello World')
     print('Hello World')
-    print('Hello World')
+    print('Hello World")
 
 if __name__ == '__main__':
     main()
